6526e7524d384ce544bd4034976c7e81e07a4961
emachipana <emachipana0102@gmail.com>
2024-08-26 11:36:14 -0500

    ADD: vitro orders entity

--- src/main/java/com/inversionesaraujo/api/model/entity/OrderVariety.java
+++ src/main/java/com/inversionesaraujo/api/model/entity/OrderVariety.java
@@ -1,16 +1,12 @@
 package com.inversionesaraujo.api.model.entity;
 
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
@@ -23,17 +19,23 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 @Builder
-@Table(name = "varieties")
-public class Variety {
+@Table(name = "order_varieties")
+public class OrderVariety {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @ManyToOne
+    @JoinColumn(name = "vitro_order_id")
+    @NotEmpty(message = "El id del pedido invitro no puede ir vacio")
+    private VitroOrder vitroOrder;
+    @ManyToOne
+    @JoinColumn(name = "variety_id")
+    @NotEmpty(message = "El id de la variedad no puede ir vacia")
+    private Variety variety;    
     @NotEmpty(message = "El precio no puede ir vacio")
     private Double price;
-    @ManyToOne
-    @JoinColumn(name = "tuber_id")
-    @NotEmpty(message = "El id del tuberculo no pueder ir vacio")
-    private Tuber tuber;
-    @OneToMany(mappedBy = "variety", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<OrderVariety> orderVarieties;
+    @NotEmpty(message = "La cantidad no puede ir vacia")
+    private Integer quantity;
+    @Column(nullable = false)
+    private Double subTotal;
 }
