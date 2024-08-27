e68b6c5a4e03aa3a3f9bda5741256c3a5134719b
emachipana <emachipana0102@gmail.com>
2024-08-26 20:01:40 -0500

    ADD: user entity and its roles

---
+++ src/main/java/com/inversionesaraujo/api/model/entity/Admin.java
@@ -0,0 +1,41 @@
+package com.inversionesaraujo.api.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Table(name = "admins")
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NotEmpty(message = "Los nombres no pueden ir vacios")
+    @Size(min = 3, max = 100)
+    private String firstName;
+    @NotEmpty(message = "Los apellidos no pueden ir vacios")
+    @Size(min = 3, max = 100)
+    private String lastName;
+    @NotEmpty(message = "El email no puede ir vacio")
+    @Email(message = "El formato es incorrecto")
+    private String email;
+    @Column(nullable = false)
+    private Double totalProfit;
+    @OneToOne(mappedBy = "admin")
+    private User user;
+}
---
+++ src/main/java/com/inversionesaraujo/api/model/entity/Client.java
@@ -0,0 +1,56 @@
+package com.inversionesaraujo.api.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Table(name = "clients")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NotEmpty(message = "La direccion no puede ir vacia")
+    @Size(min = 3)
+    private String address;
+    @NotEmpty(message = "El departamento no puede ir vacio")
+    @Size(min = 3, max = 50)
+    private String department;
+    @NotEmpty(message = "La ciudad no puede ir vacia")
+    @Size(min = 3, max = 50)
+    private String city;
+    @NotEmpty(message = "El telefono no puede ir vacio")
+    @Size(max = 12)
+    private String phone;
+    @NotEmpty(message = "El documento no puede ir vacio")
+    @Size(max = 20)
+    private String document;
+    @Column(nullable = false)
+    private Double consumption;
+    @NotEmpty(message = "Los nombres no pueden ir vacios")
+    @Size(min = 3, max = 100)
+    private String firstName;
+    @NotEmpty(message = "Los apellidos no pueden ir vacios")
+    @Size(min = 3, max = 100)
+    private String lastName;
+    @NotEmpty(message = "El email no puede ir vacio")
+    @Email(message = "El formato es incorrecto")
+    private String email;
+    @OneToOne(mappedBy = "client")
+    private User user;
+}
