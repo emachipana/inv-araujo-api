e277bb8b252a3af0420b2c9582ceb6b0770bd948
emachipana <emachipana0102@gmail.com>
2024-09-05 01:05:30 -0500

    ADD: invoice controller

--- src/main/java/com/inversionesaraujo/api/model/entity/InvoiceItem.java
+++ src/main/java/com/inversionesaraujo/api/model/entity/InvoiceItem.java
@@ -1,5 +1,7 @@
 package com.inversionesaraujo.api.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,7 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -26,20 +28,20 @@ public class InvoiceItem {
     private Integer id;
     @ManyToOne
     @JoinColumn(name = "invoice_id")
-    @NotEmpty(message = "El id del comprobante no puede ir vacio")
+    @NotNull(message = "El id del comprobante no puede ir vacio")
+    @JsonIgnore
     private Invoice invoice;
     @ManyToOne
     @JoinColumn(name = "product_id")
-    @NotEmpty(message = "El id del producto no puede ir vacio")
+    @NotNull(message = "El id del producto no puede ir vacio")
     private Product product;
-    @NotEmpty(message = "La cantidad no puede ir vacia")
+    @NotNull(message = "La cantidad no puede ir vacia")
     private Integer quantity;
     @Column(nullable = false)
     private Double subTotal;
-    @NotEmpty(message = "El precio no puede ir vacio")
+    @NotNull(message = "El precio no puede ir vacio")
     private Double price;
-    @Column(nullable = false) // si no obtenemos nada en el body request es true
-    private boolean isIgvApply;
     @Column(nullable = false)
-    private String productName;
+    @Builder.Default
+    private boolean isIgvApply = true;
 }
--- src/main/java/com/inversionesaraujo/api/service/I_Invoice.java
+++ src/main/java/com/inversionesaraujo/api/service/I_Invoice.java
@@ -3,6 +3,7 @@ package com.inversionesaraujo.api.service;
 import java.util.List;
 
 import com.inversionesaraujo.api.model.entity.Invoice;
+import com.inversionesaraujo.api.model.payload.FileResponse;
 
 public interface I_Invoice {
     List<Invoice> listAll();
@@ -12,4 +13,6 @@ public interface I_Invoice {
     Invoice findById(Integer id);
 
     void delete(Invoice invoice);
+
+    public FileResponse generateAndUploadPDF(Invoice invoice);
 }
