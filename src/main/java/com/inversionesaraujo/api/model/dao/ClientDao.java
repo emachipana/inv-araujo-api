aa851670bd26718245fdd946afc8850c5ccc5309
emachipana <emachipana0102@gmail.com>
2024-09-10 20:17:22 -0500

    ADD: auth controller

--- src/main/java/com/inversionesaraujo/api/model/dao/ClientDao.java
+++ src/main/java/com/inversionesaraujo/api/model/dao/ClientDao.java
@@ -1,7 +1,11 @@
 package com.inversionesaraujo.api.model.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.inversionesaraujo.api.model.entity.Client;
 
-public interface ClientDao extends JpaRepository<Client, Integer> {}
+public interface ClientDao extends JpaRepository<Client, Integer> {
+    Optional<Client> findById(Integer id);
+}
