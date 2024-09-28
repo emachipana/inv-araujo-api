94a131b6a60862f125809c65cecd8f48c8e58c35
emachipana <emachipana0102@gmail.com>
2024-09-27 22:55:26 -0500

    ADD: document type to client

--- src/main/java/com/inversionesaraujo/api/model/entity/Client.java
+++ src/main/java/com/inversionesaraujo/api/model/entity/Client.java
@@ -7,6 +7,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,6 +19,7 @@ import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -48,6 +51,9 @@ public class Client {
     @NotEmpty(message = "El documento no puede ir vacio")
     @Size(max = 20)
     private String document;
+    @NotNull(message = "El tipo de documento no puede ir vacio")
+    @Enumerated(EnumType.STRING)
+    private DocumentType documentType;
     @Column(nullable = false)
     private Double consumption;
     @NotEmpty(message = "Los nombres no pueden ir vacios")
