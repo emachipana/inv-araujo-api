421d6cb04c699c60e9c3b4f90ce07063aa78a8dd
emachipana <emachipana0102@gmail.com>
2024-10-29 15:53:41 -0500

    ADD: generate code and valid code funtions

--- src/main/java/com/inversionesaraujo/api/service/impl/ResetImpl.java
+++ src/main/java/com/inversionesaraujo/api/service/impl/ResetImpl.java
@@ -1,5 +1,9 @@
 package com.inversionesaraujo.api.service.impl;
 
+import java.util.Random;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
@@ -30,5 +34,31 @@ public class ResetImpl implements IReset {
     @Override
     public void delete(Reset reset) {
         resetDao.delete(reset);
+    }
+
+    @Override
+    public String generateCode() {
+        String values = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        int size = 6;
+        StringBuilder code = new StringBuilder(size);
+        Random random = new Random();
+
+        for(int i = 0; i < size; i++) {
+            int ranIndex = random.nextInt(values.length());
+            code.append(values.charAt(ranIndex));
+        }
+
+        return code.toString();
+    }
+
+    @Override
+    public Boolean validCode(Reset reset, String code) throws Exception {
+        LocalDateTime now = LocalDateTime.now(ZoneId.of("America/Lima"));
+
+        if(!code.equals(reset.getCode())) return false;
+
+        if(now.isAfter(reset.getExpiresAt())) throw new Exception("El codigo expiro");
+
+        return true;
     }
 }
