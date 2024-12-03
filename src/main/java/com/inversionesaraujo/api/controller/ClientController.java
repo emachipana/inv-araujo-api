ceaac9e9a009d824dc3ccc9f8d69bab255efe941
emachipana <emachipana0102@gmail.com>
2024-12-03 16:21:08 -0500

    ADD: pagination and sort by consumption clients

--- src/main/java/com/inversionesaraujo/api/controller/ClientController.java
+++ src/main/java/com/inversionesaraujo/api/controller/ClientController.java
@@ -3,6 +3,7 @@ package com.inversionesaraujo.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.inversionesaraujo.api.model.entity.Client;
+import com.inversionesaraujo.api.model.entity.SortDirection;
 import com.inversionesaraujo.api.model.payload.MessageResponse;
 import com.inversionesaraujo.api.service.IClient;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,15 +29,18 @@ public class ClientController {
     private IClient clientService;
 
     @GetMapping
-    public List<Client> getAll() {
-        return clientService.listAll();
+    public Page<Client> getAll(
+        @RequestParam(defaultValue = "0") Integer page,
+        @RequestParam(defaultValue = "20") Integer size,
+        @RequestParam(required = false) SortDirection sort
+    ) {
+        return clientService.filterClients(page, size, sort);
     }
 
     @GetMapping("/search")
     public List<Client> search(@RequestParam String param) {
-        return clientService.search(param, param);
+        return clientService.search(param, param, param, param);
     }
-    
 
     @GetMapping("{id}")
     public ResponseEntity<MessageResponse> getOneById(@PathVariable Integer id) {
