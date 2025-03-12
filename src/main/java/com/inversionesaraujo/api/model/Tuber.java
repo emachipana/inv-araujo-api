351b949e4d546c3aaa87ac160b2e636ebcb39746
emachipana <emachipana0102@gmail.com>
2025-03-12 18:09:51 -0500

    ADD: varieties to tuber dto

--- src/main/java/com/inversionesaraujo/api/model/Tuber.java
+++ src/main/java/com/inversionesaraujo/api/model/Tuber.java
@@ -1,10 +1,15 @@
 package com.inversionesaraujo.api.model;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,4 +29,7 @@ public class Tuber {
 
     @Column(nullable = false)
     private String name;
+
+    @OneToMany(mappedBy = "tuber", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Variety> varieties;
 }
